49f481f3f0778012ee23f41768164a73a6e37e41
naagaraa <ekabersinar@gmail.com>
2022-06-15 21:07:41 +0700

    :update readme

--- src/metode/weight-moving-average/MetodeWMA.php
+++ src/metode/weight-moving-average/MetodeWMA.php
@@ -1,4 +1,33 @@
 <?php
+
+/**
+ *
+ * this file is single method of PHP Technique for Single Moving Average
+ *
+ *
+ * @author      Eka Jaya Nagara <ekabersinar@gmail.com>
+ * @copyright   Copyright (c), 2022 naagaraa metode skripsi Technique for Wieght Moving Average
+ * @license     MIT public license
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace Nagara\Src\Metode;
 
 class MetodeWMA
